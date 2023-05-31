7f211be540b99a391845c73c639e85b4286f6513
Proyekt <ro.romanromov@yandex.ru>
2023-05-31 18:02:05 +0300

    Proyerr

---
+++ app/src/main/java/com/example/bsstatsneplagiat/activity_second.java
@@ -0,0 +1,14 @@
+package com.example.bsstatsneplagiat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
